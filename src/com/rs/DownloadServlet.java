fe9b60cedbfce91cdda67b772765b8afeb95e5e1
Robin-611 <rsrajpoot611@gmail.com>
2019-06-22 13:09:16 +0530

    first commit

---
+++ src/com/rs/DownloadServlet.java
@@ -0,0 +1,34 @@
+package com.rs;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DownloadServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        System.out.println("download servlet----");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		 File directory = new File("C:\\");
+         String files = "keywords_20190620150953.zip";
+		  response.setContentType("APPLICATION/OCTET-STREAM"); 
+		  response.setHeader("Content-Disposition","attachment; filename=\"" + files + "\""); 
+
+		  java.io.FileInputStream fileInputStream = new java.io.FileInputStream(directory.getPath()+"\\" + files);
+		  
+		  int i; 
+		  while ((i=fileInputStream.read()) != -1) {
+		    out.write(i); 
+		  } 
+		  fileInputStream.close(); 
+		  out.close(); 
+	}
+
+}
